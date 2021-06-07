684e0616e0e740cf999b75ee24e886003138a57f
mosaab-ss <80281522+mosaab-ss@users.noreply.github.com>
2021-06-06 23:55:16 -0600

    ITB-197: fixed param mismatch with ControllerAdvice

--- src/main/java/com/inthebytes/searchservice/exception/GlobalControllerAdvice.java
+++ src/main/java/com/inthebytes/searchservice/exception/GlobalControllerAdvice.java
@@ -3,6 +3,7 @@ package com.inthebytes.searchservice.exception;
 import org.hibernate.ObjectNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -12,13 +13,19 @@ public class GlobalControllerAdvice {
 
 	@ExceptionHandler(ObjectNotFoundException.class)
 	@ResponseStatus(HttpStatus.NOT_FOUND)
-	public ResponseEntity<String> handleObjectNotFound(RuntimeException ex) {
+	public ResponseEntity<String> handleObjectNotFound(ObjectNotFoundException ex) {
 		return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
 	}
 
-	@ExceptionHandler(Exception.class)
+	@ExceptionHandler(MissingServletRequestParameterException.class)
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	public ResponseEntity<String> handleBadParams(MissingServletRequestParameterException ex) {
+		return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+	}
+
+	@ExceptionHandler({Exception.class})
 	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-	public ResponseEntity<String> handleOtherException(RuntimeException ex) {
+	public ResponseEntity<String> handleOtherException(Exception ex) {
 		return new ResponseEntity<>("Something went wrong.", HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 
